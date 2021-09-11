d2b22287f343aab575e3434892c49013bb81f49c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-09-11 16:12:43 +0900

    fix norm error

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynakamot <ynakamot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/11 16:09:01 by ynakamot          #+#    #+#             */
+/*   Updated: 2021/09/11 16:11:07 by ynakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -6,10 +18,11 @@
 # endif
 
 # include <signal.h>
-# include <stdlib.h>
 # include <stdbool.h>
+# include <stdlib.h>
 # include <sys/types.h>
 # include <unistd.h>
+
 # include "./libft/libft.h"
 
 #endif
